fbf3e48e516b16e0b8e6b4c45bc5de163b5c5a97
wogml3270 <wogml3270@naver.com>
2025-05-22 17:49:06 +0900

    [faet]: 최종 병합 완료

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,17 @@ import "@/styles/globals.css";
 import Header from "@/pages/components/Header";
 import Footer from "@/pages/components/Footer";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const isAdminRoute = router.pathname.startsWith("/admin");
+
   return (
     <>
-      <Header />
+      {!isAdminRoute && <Header />}
       <Component {...pageProps} />
-      <Footer />
+      {!isAdminRoute && <Footer />}
     </>
   );
 }
---
+++ src/pages/admin/fabric/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import FabricEditor from "@/components/admin/editor/fabric";
+
+export default function Fabric() {
+  return <FabricEditor />;
+}
